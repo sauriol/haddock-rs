dc452b297178fe41d7f16f993b56bed1977a419b
Jack Sauriol <sauriol@csh.rit.edu>
2019-02-08 17:21:00 -0500

    Add printing multiple passwords

--- src/main.rs
+++ src/main.rs
@@ -21,13 +21,32 @@ fn main() {
              .value_name("length")
              .help("Set a password length. Default: 20")
              .takes_value(true))
+        .arg(Arg::with_name("count")
+             .short("n")
+             .long("number")
+             .value_name("count")
+             .help("The number of passwords to print. Default: 1")
+             .takes_value(true))
         .get_matches();
 
 
-    let file = matches.value_of("file").unwrap_or("2of12.txt");
-    let length = matches.value_of("length").unwrap_or("20").parse::<usize>().unwrap();
+    let file = matches.value_of("file")
+        .unwrap_or("2of12.txt");
+    let length = matches.value_of("length")
+        .unwrap_or("20")
+        .parse::<usize>()
+        .unwrap();
+    let count = matches.value_of("count")
+        .unwrap_or("1")
+        .parse::<usize>()
+        .unwrap();
 
     let fish = Haddock::new(length, file);
 
-    println!("Password: {}", fish.generate().unwrap());
+    for _ in 0..count {
+        match fish.generate() {
+            Some(pass)  => println!("{}", pass),
+            None        => panic!("Could not generate password"),
+        };
+    }
 }
